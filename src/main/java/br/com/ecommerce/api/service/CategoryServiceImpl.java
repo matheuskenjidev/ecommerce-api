cfdaa159caf78bd9b6a1daf08607f67297e96034
Matheus Kenji <matheuskenjidev@gmail.com>
2026-08-20 16:57:04 -0300

    refactor: adapta CategoryServiceImp para persistência com JPA

--- src/main/java/br/com/ecommerce/api/service/CategoryServiceImpl.java
+++ src/main/java/br/com/ecommerce/api/service/CategoryServiceImpl.java
@@ -30,32 +30,24 @@ public class CategoryServiceImpl implements CategoryService{
 
     @Override
     public String deleteCategory(Long categoryId) {
-        List<Category> categories = categoryRepository.findAll();
+        Category category = categoryRepository.findById(categoryId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "categoria nao encontrada, insira um id valido"));
 
-        Category category = categories.stream()
-                .filter(c -> c.getCategoryId().equals(categoryId))
-                .findFirst()
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "categoria n encontrada"));
 
         categoryRepository.delete(category);
-        return "categoria deletada";
+        return "Categoria com o Id " + category.getCategoryId() + " foi deletada";
     }
 
     @Override
     public Category updateCategory(Category category, Long categoryId) {
-        List<Category> categories = categoryRepository.findAll();
-        Optional<Category> optionalCategory = categories.stream()
-                .filter(c -> c.getCategoryId().equals(categoryId))
-                .findFirst();
 
-        if(optionalCategory.isPresent()) {
-            Category existingCategory = optionalCategory.get();
-            existingCategory.setCategoryName(category.getCategoryName());
-            Category savedCategory = categoryRepository.save(existingCategory);
+        Category savedCategory = categoryRepository.findById(categoryId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Categoria nao encontrada, insira um id valido"));
 
-            return savedCategory;
-        } else {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "categoria nao encontrada, insira um id correto");
-        }
+
+        category.setCategoryId(categoryId);
+        savedCategory = categoryRepository.save(category);
+        return savedCategory;
+
     }
 }
